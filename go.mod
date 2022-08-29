317eaaf9b5ab970b468537b5d2a7b36649e101d8
romeq <toke8677@gmail.com>
2022-08-29 12:12:03 +0300

    add tests

---
+++ go.mod
@@ -0,0 +1,31 @@
+module github.com/romeq/tapsa
+
+go 1.19
+
+require github.com/stretchr/testify v1.8.0
+
+require (
+	github.com/BurntSushi/toml v1.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.8.1 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
+	golang.org/x/crypto v0.0.0-20220826181053-bd7e27e6170d // indirect
+	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
+	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"io"
+	"mime/multipart"
+	"net/http/httptest"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/romeq/tapsa/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpload(t *testing.T) {
+	cfg := config.New("127.0.0.1", 8080, []string{""}, false, 20, "uploads")
+	testfileName := path.Join(cfg.Files.UploadsDir, "testfile")
+	pr, pw := io.Pipe()
+
+	// setup writers
+	errhandle(t, os.MkdirAll(cfg.Files.UploadsDir, 0755))
+	testfile, err := os.Create(testfileName)
+	errhandle(t, err)
+	defer testfile.Close()
+
+	writer := multipart.NewWriter(pw)
+	go func() {
+		defer writer.Close()
+
+		part, err := writer.CreateFormFile("file", testfile.Name())
+		errhandle(t, err)
+		_, err = part.Write([]byte("testtesttest!"))
+		errhandle(t, err)
+	}()
+
+	request := httptest.NewRequest("POST", "/api/file/upload", pr)
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+
+	response := httptest.NewRecorder()
+	r := setuprouter(cfg)
+	r.ServeHTTP(response, request)
+
+	// test output
+	assert.Equal(t, 200, response.Code)
+
+	uploadedFile, err := os.Open("uploads/testfile")
+	errhandle(t, err)
+	fileEquals(t, testfile, uploadedFile)
+
+	t.Cleanup(func() {
+		err := os.Remove("uploads/testfile")
+		errhandle(t, err)
+	})
+}
+
+func fileEquals(t *testing.T, z *os.File, f *os.File) {
+	zc, err := io.ReadAll(z)
+	errhandle(t, err)
+	fc, err := io.ReadAll(f)
+	errhandle(t, err)
+	assert.Equal(t, zc, fc)
+}
+
+func errhandle(t *testing.T, err error) {
+	if err != nil {
+		t.Error(err)
+	}
+}
