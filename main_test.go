1d76e5acb45e7da8cb231e61973b85538cf38209
romeq <toke8677@gmail.com>
2022-09-06 10:19:58 +0300

    make tests cleaner

--- main_test.go
+++ main_test.go
@@ -3,38 +3,96 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"log"
 	"mime/multipart"
 	"net/http/httptest"
 	"os"
 	"path"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/romeq/tapsa/config"
 	"github.com/romeq/tapsa/dbengine"
 	"github.com/stretchr/testify/assert"
 )
 
-func initdb(t *testing.T) {
+var (
+	r       = &gin.Engine{}
+	workdir = ""
+	cfg     = config.Config{
+		Server: config.Server{
+			Address:        "127.0.0.1",
+			Port:           8080,
+			TrustedProxies: []string{"127.0.0.1"},
+			DebugMode:      false,
+			HideRequests:   true,
+		},
+		Files: config.Files{
+			MaxSize:    0,
+			UploadsDir: "uploads",
+		},
+	}
+)
+
+func prepareWorkspace() {
+	setuplogger()
+
+	tmpdir, err := os.MkdirTemp("", "tapsa")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	err = os.Chdir(tmpdir)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	workdir = tmpdir
+	if err = os.MkdirAll(cfg.Files.UploadsDir, 0755); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	initdb()
+	r = setuprouter(cfg)
+}
+
+func initdb() {
 	f, err := os.Create("test.db")
 	if err != nil {
-		t.Error("Failed to create database file:", err)
+		log.Fatalln("Failed to create database file:", err)
 	}
 	f.Close()
 	dbengine.Init("test.db")
 }
 
+func fileContentEquals(t *testing.T, z string, f *os.File) {
+	fc, err := io.ReadAll(f)
+	errhandle(t, err)
+	assert.Equal(t, z, string(fc))
+}
+
+func errhandle(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal("test resulted in an error: ", err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	prepareWorkspace()
+	result := m.Run()
+
+	// cleanup
+	if err := os.Remove("test.db"); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	os.Exit(result)
+}
+
 func TestUpload(t *testing.T) {
-	initdb(t)
-	setuplogger()
-
-	cfg := config.New("127.0.0.1", 8080, []string{"127.0.0.1"}, false, true, 20, "uploads")
-	testFileName := path.Join("tmp", "testfile")
-	testFileContent := `testtesttest!`
 	pr, pw := io.Pipe()
+	testFileName := "testfile"
+	testFileContent := `jarkko bought a beer; 2.50emarikka bought a dress; 50e`
 
-	// setup writers
-	errhandle(t, os.MkdirAll(cfg.Files.UploadsDir, 0755))
-	errhandle(t, os.MkdirAll("tmp", 0755))
 	testfile, err := os.Create(testFileName)
 	errhandle(t, err)
 	defer testfile.Close()
@@ -49,41 +107,27 @@ func TestUpload(t *testing.T) {
 		errhandle(t, err)
 	}()
 
-	r := setuprouter(cfg)
 	request := httptest.NewRequest("POST", "/api/file/upload", pr)
 	request.Header.Add("Content-Type", writer.FormDataContentType())
 
 	response := httptest.NewRecorder()
 	r.ServeHTTP(response, request)
 
-	// test output
+	// assert output
 	assert.Equal(t, 200, response.Code)
 
 	output := map[string]string{}
 	errhandle(t, json.Unmarshal(response.Body.Bytes(), &output))
 	filepath := path.Join(cfg.Files.UploadsDir, output["filename"])
+
 	uploadedFile, err := os.Open(filepath)
 	errhandle(t, err)
 	fileContentEquals(t, testFileContent, uploadedFile)
 
 	t.Cleanup(func() {
-		err := os.Remove("test.db")
-		errhandle(t, err)
-		err = os.Remove(path.Join("tmp", "testfile"))
+		err = os.Remove(path.Join(workdir, "testfile"))
 		errhandle(t, err)
 		err = os.Remove(filepath)
 		errhandle(t, err)
 	})
-}
-
-func fileContentEquals(t *testing.T, z string, f *os.File) {
-	fc, err := io.ReadAll(f)
-	errhandle(t, err)
-	assert.Equal(t, z, string(fc))
-}
-
-func errhandle(t *testing.T, err error) {
-	if err != nil {
-		t.Fatal("test resulted in an error: ", err)
-	}
 }
