069149d4a283f425a85723132716447bad31fab3
romeq <toke8677@gmail.com>
2022-09-21 16:31:02 +0300

    use sql queries

--- dbengine/files.go
+++ dbengine/files.go
@@ -1,25 +1,9 @@
 package dbengine
 
-import "errors"
-
 // InsertFile creates a new record on file database
 // from given File struct
 func InsertFile(file File) error {
-	if err := ensureConnection(); err != nil {
-		return err
-	}
-
-	statement := `INSERT INTO files(
-		filename,
-		password,
-		upload_date,
-		file_size,
-		is_encrypted,
-		viewcount,
-		owner_id
-	) VALUES(?, ?, ?, ?, ?, ?, ?);`
-
-	if _, err := DbConnection.Exec(statement, file.Filename, file.Password,
+	if _, err := DbConnection.Exec(insertFileQuery, file.Filename, file.Password,
 		file.UploadDate, file.FileSize, file.IsEncrypted,
 		file.ViewCount, file.OwnerId); err != nil {
 		return err
@@ -30,12 +14,7 @@ func InsertFile(file File) error {
 
 // DeleteFile removes file metadata from database
 func DeleteFile(filename string) error {
-	if err := ensureConnection(); err != nil {
-		return err
-	}
-
-	stmt := "DELETE FROM files WHERE filename = ?;"
-	if _, err := DbConnection.Exec(stmt, filename); err != nil {
+	if _, err := DbConnection.Exec(deleteFileQuery, filename); err != nil {
 		return err
 	}
 
@@ -44,12 +23,7 @@ func DeleteFile(filename string) error {
 
 // GetPasswordHash returns password field from database
 func GetPasswordHash(filename string) (pwd string, err error) {
-	if err = ensureConnection(); err != nil {
-		return "", err
-	}
-
-	stmt := "SELECT password FROM files WHERE filename = ?;"
-	row := DbConnection.QueryRow(stmt, filename)
+	row := DbConnection.QueryRow(getPasswordQuery, filename)
 	err = row.Scan(&pwd)
 
 	return pwd, err
@@ -63,16 +37,12 @@ Note: only following fields are included, thus any other
 fields will remain empty:
   - Filename
   - UploadDate
+  - IsEncrypted
   - FileSize
   - ViewCount
 */
 func GetFile(filename string) (f File, err error) {
-	if err = ensureConnection(); err != nil {
-		return File{}, err
-	}
-
-	row := DbConnection.QueryRow("SELECT filename, upload_date, is_encrypted, file_size, viewcount FROM files WHERE filename = ?",
-		filename)
+	row := DbConnection.QueryRow(getFileQuery, filename)
 	if err = row.Scan(&f.Filename, &f.UploadDate, &f.IsEncrypted,
 		&f.FileSize, &f.ViewCount); err != nil {
 		return File{}, err
@@ -84,21 +54,9 @@ func GetFile(filename string) (f File, err error) {
 // IncrementFileViewCount increments file's viewcount
 // field by 1 in database
 func IncrementFileViewCount(filename string) error {
-	if err := ensureConnection(); err != nil {
+	if _, err := DbConnection.Exec(incrementFileViewCountQuery, filename); err != nil {
 		return err
 	}
 
-	statement := "UPDATE files SET viewcount = viewcount + 1 WHERE filename = ?"
-	if _, err := DbConnection.Exec(statement, filename); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func ensureConnection() error {
-	if DbConnection == nil {
-		return errors.New("database is not connected")
-	}
 	return nil
 }
