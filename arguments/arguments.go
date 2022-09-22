ec2ffa5dbbc995f7e5bbb8341e02854c2f5cbdf3
romeq <toke8677@gmail.com>
2022-09-22 12:47:06 +0300

    default values for arguments overrode cfg

--- arguments/arguments.go
+++ arguments/arguments.go
@@ -15,10 +15,10 @@ type Arguments struct {
 func Parse() Arguments {
 	args := Arguments{}
 
-	flag.StringVar(&args.Config.Server.Address, "a", "127.0.0.1", "server address")
+	flag.StringVar(&args.Config.Server.Address, "a", "", "server address")
 	flag.StringVar(&args.ConfigFile, "c", "/etc/usva/tapsa.toml", "config location")
 	flag.StringVar(&args.LogOutput, "l", "", "log location")
-	flag.IntVar(&args.Config.Server.Port, "p", 8080, "server port")
+	flag.IntVar(&args.Config.Server.Port, "p", 0, "server port")
 
 	flag.Parse()
 	return args
