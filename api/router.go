6b95a41be58e3a914cb4bbf599c1f4d92ae78d42
toke <toke8677@gmail.com>
2022-11-16 21:27:28 +0200

    fix typo

--- api/router.go
+++ api/router.go
@@ -7,7 +7,7 @@ package api
 // Limits-struct is primarily used to configure the ratelimiting functionality
 type Limits struct {
 	AllowedRequests int16 // Requests to allow
-	ResetTime       int32 // Time to next resetto next reset  in seconds
+	ResetTime       int32 // Time to next reset in seconds
 }
 
 type Ratelimits struct {
