5ed243cc580c8d62cf791719630b50a1e28aedce
toke <touko@testausserveri.fi>
2022-11-25 17:33:25 +0200

    test more ratelimit functions

--- api/middleware/ratelimit_test.go
+++ api/middleware/ratelimit_test.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -12,19 +13,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRatelimiter_RestrictUploads(t *testing.T) {
-	fakerInstance := faker.New()
+var (
+	fakerInstance = faker.New()
+)
 
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.ReleaseMode)
+	os.Exit(m.Run())
+}
+
+func TestRatelimiter_RestrictUploads(t *testing.T) {
+	t.Parallel()
 	type args struct {
-		statusWants int
 		allowedData uint64
-		wantsHeader bool
 	}
 	tests := []struct {
-		name   string
-		client Client
-		args   args
-		want   gin.HandlerFunc
+		name       string
+		client     Client
+		args       args
+		want       int
+		wantHeader bool
 	}{
 		{
 			name: "valid user",
@@ -34,9 +42,9 @@ func TestRatelimiter_RestrictUploads(t *testing.T) {
 			},
 			args: args{
 				allowedData: 32,
-				statusWants: 200,
-				wantsHeader: true,
 			},
+			want:       http.StatusOK,
+			wantHeader: true,
 		},
 		{
 			name: "invalid user",
@@ -53,9 +61,9 @@ func TestRatelimiter_RestrictUploads(t *testing.T) {
 			},
 			args: args{
 				allowedData: 32,
-				statusWants: 406,
-				wantsHeader: false,
 			},
+			want:       http.StatusNotAcceptable,
+			wantHeader: false,
 		},
 	}
 
@@ -65,7 +73,6 @@ func TestRatelimiter_RestrictUploads(t *testing.T) {
 				Clients: &[](*Client){&tt.client},
 			}
 
-			gin.SetMode(gin.ReleaseMode)
 			res := httptest.NewRecorder()
 			_, r := gin.CreateTestContext(res)
 
@@ -78,10 +85,131 @@ func TestRatelimiter_RestrictUploads(t *testing.T) {
 			req.Header.Set("Api-Identifier", tt.client.Identifier)
 			r.ServeHTTP(res, req)
 
-			assert.Equal(t, tt.args.statusWants, res.Result().StatusCode, tt.name)
-			if tt.args.wantsHeader {
+			assert.Equal(t, tt.want, res.Result().StatusCode, tt.name)
+			if tt.wantHeader {
 				assert.NotEmpty(t, res.Header().Get("Usva-AllowedUploadBytes"), tt.name)
 			}
+		})
+	}
+}
+
+func TestRequestHandler_UseToken(t *testing.T) {
+	t.Parallel()
+	type fields struct {
+		nextReset     time.Time
+		tokens        int16
+		maximumTokens int16
+	}
+	type args struct {
+		count int16
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			name: "test case 1",
+			fields: fields{
+				nextReset:     time.Now().Add(time.Hour),
+				tokens:        3,
+				maximumTokens: 3,
+			},
+			args: args{
+				count: 1,
+			},
+			want: true,
+		},
+		{
+			name: "test case 1",
+			fields: fields{
+				nextReset:     time.Now().Add(time.Hour),
+				tokens:        0,
+				maximumTokens: 3,
+			},
+			args: args{
+				count: 1,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hand := &RequestHandler{
+				nextReset:     tt.fields.nextReset,
+				tokens:        tt.fields.tokens,
+				maximumTokens: tt.fields.maximumTokens,
+			}
+			if got := hand.UseToken(tt.args.count); got != tt.want {
+				t.Errorf("RequestHandler.UseToken() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRatelimiter_RestrictRequests(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		lastCleanup      time.Time
+		allowedRequests  int16
+		testRequestCount uint
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		{
+			name: "fail-test",
+			fields: fields{
+				lastCleanup:      time.Now().Add(-time.Minute),
+				allowedRequests:  1,
+				testRequestCount: 2,
+			},
+			want: http.StatusTooManyRequests,
+		},
+		{
+			name: "success-test",
+			fields: fields{
+				lastCleanup:      time.Now().Add(-time.Minute),
+				allowedRequests:  2,
+				testRequestCount: 2,
+			},
+			want: http.StatusOK,
+		},
+		{
+			name: "success-test-nolimits",
+			fields: fields{
+				lastCleanup:      time.Now().Add(-time.Minute),
+				allowedRequests:  0,
+				testRequestCount: 10,
+			},
+			want: http.StatusOK,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			limiterBase := NewRatelimiter()
+
+			limiter := limiterBase.RestrictRequests(tt.fields.allowedRequests, time.Hour)
+
+			responseWriter := httptest.NewRecorder()
+			_, r := gin.CreateTestContext(responseWriter)
+			r.GET("/", limiter)
+
+			var res *httptest.ResponseRecorder
+			for iter := int16(0); uint(iter) < tt.fields.testRequestCount; iter++ {
+				req := httptest.NewRequest("GET", "/", strings.NewReader("hello"))
+				req.Header.Set("Api-Identifier", "api-identifier")
+
+				res = httptest.NewRecorder()
+				r.Handler().ServeHTTP(res, req)
+			}
+			statusgot := res.Code
+			assert.Equal(t, tt.want, statusgot)
+
 		})
 	}
 }
