2f81a935a43997c87ec6f9bb4bb4a1a5cd942e5e
toke <touko@testausserveri.fi>
2022-11-27 20:01:12 +0200

    save file authorization to a temporary cookie

--- sqlc/schemas/file.sql
+++ sqlc/schemas/file.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS file(
     title VARCHAR(256),
     passwdhash VARCHAR(512),
     uploader VARCHAR(256),
+    access_token VARCHAR(40) NOT NULL UNIQUE,
     file_uuid VARCHAR(256) NOT NULL UNIQUE,
     isencrypted BOOLEAN NOT NULL DEFAULT FALSE,
     upload_date VARCHAR(256) NOT NULL,
