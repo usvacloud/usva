cdb1a7af1cd66f55ededc51d80f8443b12b96617
toke <touko@testausserveri.fi>
2022-11-27 21:59:56 +0200

    fix reference

--- sqlc/schemas/report.sql
+++ sqlc/schemas/report.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS report(
     file_uuid VARCHAR(256) NOT NULL,
     reason VARCHAR(1024) NOT NULL,
-    FOREIGN KEY (file_uuid) REFERENCES files(file_uuid)
+    FOREIGN KEY (file_uuid) REFERENCES file(file_uuid)
 );
