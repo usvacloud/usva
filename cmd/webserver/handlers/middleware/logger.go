e5d874120642bb784a2bacbd366e39751fa56ebb
toke <touko@testausserveri.fi>
2023-02-12 12:11:41 +0200

    style: change logging style

--- cmd/webserver/handlers/middleware/logger.go
+++ cmd/webserver/handlers/middleware/logger.go
@@ -10,8 +10,8 @@ import (
 func (s *Handler) Log(ctx *gin.Context) {
 	t := time.Now()
 	ctx.Next()
-	c := time.Since(t).Milliseconds()
+	c := time.Since(t).Microseconds()
 
-	log.Printf("request: %s %s %d (took %dms) \n",
-		ctx.Request.Method, ctx.Request.URL, ctx.Writer.Status(), c)
+	log.Printf("'%s %s' %d @ [%.2fms / %dµs]\n",
+		ctx.Request.Method, ctx.Request.URL, ctx.Writer.Status(), float64(c)/1000, c)
 }
